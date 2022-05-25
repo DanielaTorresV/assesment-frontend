b8ddaaebed6c93ee4d081ce004b511ab04be5308
Daniela <dannytorres0211@gmail.com>
2022-05-24 20:08:22 -0500

    Add component Time to counterDown on ProductCard

--- src/components/Home/Time.js
+++ src/components/Home/Time.js
@@ -1,15 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Time = () => {
-  const [count, setCount] = useState(0);
+  // We need ref in this, because we are dealing
+  // with JS setInterval to keep track of it and
+  // stop it when needed
+  const Ref = useRef(null);
+
+  // The state for our timer
+  const [timer, setTimer] = useState('00:00:00');
+
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor(((total / 1000) * 60 * 60) % 24);
+    return {
+      total,
+      hours,
+      minutes,
+      seconds,
+    };
+  };
+
+  const startTimer = (e) => {
+    let { total, hours, minutes, seconds } = getTimeRemaining(e);
+    if (total >= 0) {
+      // update the timer
+      // check if less than 10 then we need to
+      // add '0' at the begining of the variable
+      setTimer(
+        (hours > 9 ? hours : '0' + hours) +
+          ':' +
+          (minutes > 9 ? minutes : '0' + minutes) +
+          ':' +
+          (seconds > 9 ? seconds : '0' + seconds)
+      );
+    }
+  };
+
+  const clearTimer = (e) => {
+    const id = setInterval(() => {
+      startTimer(e);
+    }, 1000);
+    Ref.current = id;
+  };
+
+  const getDeadTime = () => {
+    let deadline = new Date();
+
+    deadline.setSeconds(deadline.getSeconds() + Math.random() * 60);
+    deadline.setMinutes(deadline.getMinutes() + Math.random() * 2);
+    return deadline;
+  };
 
   useEffect(() => {
-    setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 1000);
+    clearTimer(getDeadTime());
   }, []);
 
-  return <h5>{count}</h5>;
+  return (
+    <div className="App">
+      <h2>{timer}</h2>
+    </div>
+  );
 };
-
 export default Time;
